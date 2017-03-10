1701e0f0e0687d6a20bb2df5afdc18a004d4b787
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2017-03-10 17:14:28 -0300

    .

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,6 +34,7 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Cors::class,
         ],
 
         'api' => [
