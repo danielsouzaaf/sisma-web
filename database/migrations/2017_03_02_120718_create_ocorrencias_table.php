361295f7fd6793ba6268b08e9a831431fd5bf8f6
Daniel Souza Affonso Ferreira <daniel.souza.af@mail.com>
2017-03-20 20:37:57 -0300

    fix migrations

--- database/migrations/2017_03_02_120718_create_ocorrencias_table.php
+++ database/migrations/2017_03_02_120718_create_ocorrencias_table.php
@@ -16,7 +16,7 @@ class CreateOcorrenciasTable extends Migration
         Schema::create('ocorrencias', function (Blueprint $table) {
             $table->increments('id');
             $table->text('local');
-            $table->text('descricao')->unsigned();
+            $table->text('descricao');
             $table->text('foto')->nullable();
             $table->integer('solicitante_id')->unsigned();
             $table->timestamps();
